40ba34ca408d063dd838398a3a1bd8463d26858e
shenshuai_dr <shen-sh@neusoft.com>
2024-03-28 18:57:17 +0800

    content显示弹窗

--- chrome3/content.js
+++ chrome3/content.js
@@ -1,15 +1,36 @@
 console.log('content.js hello everybody!');
 
+
 let newDiv = document.createElement('div');
-newDiv.innerHTML = 'hello everybody!';
+newDiv.innerHTML = `<div id="wrapper">
+  <h3>小仙女～消消气</h3>
+  <div><button id="cancel">已消气</button>
+  <button id="reject">不原谅</button></div>
+</div>`;
 newDiv.id = 'newDiv';
 document.body.appendChild(newDiv);
+const cancelBtn = document.querySelector('#cancel');
+const rejectBtn = document.querySelector('#reject');
+cancelBtn.onclick = function() {
+  document.body.removeChild(newDiv);
+  chrome.storage.sync.set({ state: 'cancel' }, (data) => {
+  });
+}
+rejectBtn.onclick = function() {
+  newDiv.style.bottom = Math.random() * 200 + 10 + "px";
+  newDiv.style.right = Math.random() * 800 + 10 + "px";
+}
+// chrome.storage.sync.get({ state: '' }, (data) => {
+//   if (data.state === 'cancel') {
+//     document.body.removeChild(newDiv);
+//   }
+// });
 
 chrome.runtime.sendMessage('get-user-data', (response) => {
   // 3. Got an asynchronous response with the data from the service worker
   console.log('received user data', response);
   //   initializeUI(response);
-  newDiv.innerHTML = response;
+  // newDiv.innerHTML = response;
 });
 chrome.storage.sync.get({ namespaced: 'aaa' }, (data) => {
   console.log(data, 'namespaced from background js');
